b9193b471fad6b99aa8ad2a2b06453a2396f6ee0
Robert Pack <robert.pack@basf.com>
2022-07-24 21:27:20 +0200

    feat: add basic router navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,12 @@
-import {
-  AppstoreOutlined,
-  BarChartOutlined,
-  CloudOutlined,
-  ShopOutlined,
-  TeamOutlined,
-  UploadOutlined,
-  UserOutlined,
-  VideoCameraOutlined,
-} from "@ant-design/icons";
 import type { MenuProps } from "antd";
-import { Layout, Menu } from "antd";
+import { Layout, Menu, Button, Descriptions, PageHeader } from "antd";
+import { Route, Link, Routes } from "react-router-dom";
 import React from "react";
-import "antd/dist/antd.css";
+import Plot from "react-plotly.js";
 import "./App.css";
+import { PieChartTwoTone, DesktopOutlined } from "@ant-design/icons";
 
-const { Header, Content, Footer, Sider } = Layout;
-
-const items: MenuProps["items"] = [
-  UserOutlined,
-  VideoCameraOutlined,
-  UploadOutlined,
-  BarChartOutlined,
-  CloudOutlined,
-  AppstoreOutlined,
-  TeamOutlined,
-  ShopOutlined,
-].map((icon, index) => ({
-  key: String(index + 1),
-  icon: React.createElement(icon),
-  label: `nav ${index + 1}`,
-}));
+const { Content, Sider } = Layout;
 
 const App: React.FC = () => (
   <Layout hasSider>
@@ -44,37 +21,96 @@ const App: React.FC = () => (
       }}
     >
       <div className="logo" />
-      <Menu
-        theme="dark"
-        mode="inline"
-        defaultSelectedKeys={["4"]}
-        items={items}
-      />
+      <Menu theme="dark" mode="inline" defaultSelectedKeys={["1"]}>
+        <Menu.Item key="1">
+          <PieChartTwoTone />
+          <span>Deshboard</span>
+          <Link to="/" />
+        </Menu.Item>
+        <Menu.Item key="2">
+          <DesktopOutlined />
+          <span>Meseros</span>
+          <Link to="/meseros" />
+        </Menu.Item>
+      </Menu>
     </Sider>
-    <Layout className="site-layout" style={{ marginLeft: 200 }}>
-      <Header className="site-layout-background" style={{ padding: 0 }} />
-      <Content style={{ margin: "24px 16px 0", overflow: "initial" }}>
-        <div
-          className="site-layout-background"
-          style={{ padding: 24, textAlign: "center" }}
-        >
-          <p>long content</p>
-          {
-            // indicates very long content
-            Array.from({ length: 100 }, (_, index) => (
-              <React.Fragment key={index}>
-                {index % 20 === 0 && index ? "more" : "..."}
-                <br />
-              </React.Fragment>
-            ))
-          }
-        </div>
+    <Layout
+      className="site-layout"
+      style={{ marginLeft: 200, height: "100vh" }}
+    >
+      <PageHeader
+        ghost={false}
+        onBack={() => window.history.back()}
+        title="Title"
+        subTitle="This is a subtitle"
+        extra={[
+          <Button key="1" type="primary">
+            Primary
+          </Button>,
+        ]}
+      >
+        <Descriptions size="small" column={3}>
+          <Descriptions.Item label="Created">Lili Qu</Descriptions.Item>
+          <Descriptions.Item label="Association">
+            <a>421421</a>
+          </Descriptions.Item>
+          <Descriptions.Item label="Creation Time">
+            2017-01-10
+          </Descriptions.Item>
+          <Descriptions.Item label="Effective Time">
+            2017-10-10
+          </Descriptions.Item>
+          <Descriptions.Item label="Remarks">
+            Gonghu Road, Xihu District, Hangzhou, Zhejiang, China
+          </Descriptions.Item>
+        </Descriptions>
+      </PageHeader>
+      <Content>
+        <Routes>
+          <Route path="/" element={<PlotElem />} />
+          <Route path="meseros" element={<PlotElem2 />} />
+        </Routes>
       </Content>
-      <Footer style={{ textAlign: "center" }}>
-        Ant Design ©2018 Created by Ant UED
-      </Footer>
     </Layout>
   </Layout>
+);
+
+const PlotElem: React.FC = () => (
+  <Plot
+    style={{ width: "100%", height: "100%" }}
+    config={{ responsive: true }}
+    useResizeHandler={true}
+    data={[
+      {
+        x: [1, 2, 3],
+        y: [2, 6, 3],
+        type: "scatter",
+        mode: "lines+markers",
+        marker: { color: "red" },
+      },
+      { type: "bar", x: [1, 2, 3], y: [2, 5, 3] },
+    ]}
+    layout={{ autosize: true }}
+  />
+);
+
+const PlotElem2: React.FC = () => (
+  <Plot
+    style={{ width: "100%", height: "100%" }}
+    config={{ responsive: true }}
+    useResizeHandler={true}
+    data={[
+      {
+        x: [1, 2, 3, 4],
+        y: [2, 6, 3, 4],
+        type: "scatter",
+        mode: "lines+markers",
+        marker: { color: "red" },
+      },
+      { type: "bar", x: [1, 2, 3], y: [2, 5, 3] },
+    ]}
+    layout={{ autosize: true }}
+  />
 );
 
 export default App;
