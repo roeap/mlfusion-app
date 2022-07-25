5ea4c2cf9aafdcdca0a955620f75b3a3547995d3
Robert Pack <robert.pack@basf.com>
2022-07-25 20:59:44 +0200

    basic asset list

--- src-tauri/src/api.rs
+++ src-tauri/src/api.rs
@@ -1,6 +1,6 @@
 use serde::{Deserialize, Serialize};
 
-use crate::models;
+use crate::models::{self, area_source_reference::Table};
 
 #[derive(Serialize, Deserialize)]
 pub struct AreaInfo {
@@ -10,12 +10,10 @@ pub struct AreaInfo {
 #[tauri::command]
 pub fn list_data_assets() -> Vec<AreaInfo> {
     let area_ref = models::AreaSourceReference {
-        table: Some(models::area_source_reference::Table::Location(
-            models::AreaTableLocation {
-                areas: vec!["Hello".to_string()],
-                name: "World".to_string(),
-            },
-        )),
+        table: Some(Table::Location(models::AreaTableLocation {
+            areas: vec!["Hello".to_string()],
+            name: "World".to_string(),
+        })),
     };
     vec![AreaInfo { source: area_ref }]
 }
--- src/routes/DataAssetCatalog.tsx
+++ src/routes/DataAssetCatalog.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { mlfusion } from "../clients";
+import { Card, List } from "antd";
 
 export const DataAssetCatalog: React.FC = () => {
   const [dataAssets, setDataAssets] = useState<mlfusion.AreaInfo[]>([]);
@@ -7,5 +8,24 @@ export const DataAssetCatalog: React.FC = () => {
   useEffect(() => {
     mlfusion.list_data_assets().then((data) => setDataAssets(data));
   }, [dataAssets]);
-  return <p>{JSON.stringify(dataAssets)}</p>;
+  return <DataAssetList assets={dataAssets} />;
+};
+
+interface DataAssetListProps {
+  assets: mlfusion.AreaInfo[];
+}
+
+const DataAssetList: React.FC<DataAssetListProps> = (props) => {
+  return (
+    <List
+      style={{ margin: 16 }}
+      grid={{ gutter: 16, column: 1 }}
+      dataSource={props.assets}
+      renderItem={(item) => (
+        <List.Item>
+          <Card title={item.source.location?.name}>Card content</Card>
+        </List.Item>
+      )}
+    />
+  );
 };
