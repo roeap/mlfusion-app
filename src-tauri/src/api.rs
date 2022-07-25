f5ef5e5b129748b89129c7411ea4fa647f71ef8d
Robert Pack <robert.pack@basf.com>
2022-07-25 19:32:50 +0200

    generate from flight protos

--- src-tauri/src/api.rs
+++ src-tauri/src/api.rs
@@ -1,14 +1,6 @@
-use serde::{Deserialize, Serialize};
-
 mod models {
     include!("flight_fusion.ipc.v1alpha1.rs");
     include!("flight_fusion.ipc.v1alpha1.serde.rs");
-}
-
-#[derive(Serialize, Deserialize, Clone)]
-pub struct Parameters {
-    pub name: String,
-    pub info: String,
 }
 
 #[tauri::command]
